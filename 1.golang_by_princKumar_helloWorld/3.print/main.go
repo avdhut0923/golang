a62de8bf7b3e825abd77679527fb640e45eda75d
avdhut0923 <122094288+avdhut0923@users.noreply.github.com>
2024-12-29 10:56:36 +0530

    Add files via upload

---
+++ 1.golang_by_princKumar_helloWorld/3.print/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 23
+	name := "jai shree ram "
+	height := 7.3332
+
+	// fmt.Println("age :", age, "name :", name, "height :", height)
+	// fmt.Println("ram ram ")
+
+	// fmt.Printf("age is %d\n", age)
+	// fmt.Printf("height is %.3f\n", height)
+	// fmt.Printf("name is %s\n", name)
+	// fmt.Printf("type of height is %T\n", height)
+
+	fmt.Printf("Name: %s, age: %d , height:%.2f", name, age, height)
+}
